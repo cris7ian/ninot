efe438012854f32fd13d79699e5a62ce9cce3a88
Cristian Caroli <>
2018-02-04 18:12:08 +0100

    looks

--- src/App.js
+++ src/App.js
@@ -81,11 +81,20 @@ class App extends Component {
           <div className="container">
             <div className="row justify-content-md-center">
               <div className="col col-lg-8">
-                <h3>The Location</h3>
-                <p>
+                <div className="description">
+                  <h3>The Location</h3>
                   Our restaurant can be found inside Mercat del Ninot, a small, yet abundant market, tucked away on the edge of Eixample, near Hospital Clinic. Situated comfortably far from the hustle and bustle of the Barcelona city center, El Ninot is still easily accessible by foot or by public transport. This vibrant, residential district, full of upscale buildings and boutiques, is a brilliant alternative to the constantly packed Gòtic and even Born, with less tourists and a much more elegant overall feel.
                   The market sells local and fresh meat, fish, vegetables, dairy and a variety of homemade delicacies. It is a place where hundreds of locals get their produce every day; clean and calm it differs greatly from other food bazaars, like Boqueria or the Mercado de Santa Caterina, which many tourists will flock to.
-                </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div id="opening-hours" className="blocks">
+          <div className="container">
+            <div className="row justify-content-md-center">
+              <div className="col col-lg-8">
 
                 <h3>Opening hours</h3>
                 <p>
