7abc9642b255f66a11dfd5598183740b9001f612
Cristian Caroli <>
2018-02-22 09:57:17 +0100

    restaurant logos

--- src/App.js
+++ src/App.js
@@ -11,6 +11,9 @@ import Map from './components/Map'
 import { FoodGallery } from './GalleryGrid'
 import {addLocaleData, FormattedMessage, IntlProvider} from 'react-intl'
 import { flattenMessages } from './utils'
+import icebarJPG from './img/restaurants/icebar.png'
+import coconutJPG from './img/restaurants/coconut.png'
+import toucheJPG from './img/restaurants/touche.png'
 import en from 'react-intl/locale-data/en'
 import es from 'react-intl/locale-data/es'
 import ca from 'react-intl/locale-data/ca'
@@ -107,7 +110,20 @@ class App extends Component {
           <Events/>
           <Contact lang={formLanguages[locale] || formLanguages['en-US']}/>
           <div id="memberLogo">
-            <img src={member[locale]}  alt="Barcelona Turismo"/>
+            <div>
+              <img src={member[locale]}  alt="Barcelona Turismo"/>
+            </div>
+            <div id="restaurantLogos">
+              <a href="http://www.touchbcn.com">
+                <img src={toucheJPG} alt="touche"/>
+              </a>
+              <a href="https://www.icebarcelona.com">
+                <img src={icebarJPG} alt="icebarcelona"/>
+              </a>
+              <a href="https://www.facebook.com/thecoconutclubbcn/">
+                <img src={coconutJPG} alt="coconut restaurant"/>
+              </a>
+            </div>
           </div>
           <Map/>
           <Footer />
